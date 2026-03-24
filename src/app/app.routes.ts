c5d6486e34ef4ad809fb9b757dc099eda997deb1
Sergey Malyuga <sergey@malyuga.ru>
2026-03-24 12:40:44 +0300

    feat(main-component): создать пустой компонент
    Файлы:
         - main-component.ts: класс компонента
         - main-component.html: шаблон
         - main-component.scss: базовые стили

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {AppRoute} from './constants/consts';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: AppRoute.MAIN,
+    title: 'MainComponent',
+    loadComponent: () => import('./pages/main/main-component').then(m => m.MainComponent),
+  }
+];
