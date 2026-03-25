249f265d79a97e5f99f000dfcbadfc616b7592d0
Sergey Malyuga <sergey@malyuga.ru>
2026-03-25 11:51:46 +0300

    feat(links.data.ts): создать константные данные для ссылок
    Создан файл с константными данными LINKS_DATA для header.component.
    Данные группируются по типу ссылки (TEXT и BUTTON).
    
    Структура:
    {
      [LinkType.TEXT]: LinkModel[],
      [LinkType.BUTTON]: LinkModel[]
    }
    
    Используется в header.component для отрисовки навигации

--- src/app/features/header/data/links.data.ts
+++ src/app/features/header/data/links.data.ts
@@ -1,25 +1,36 @@
-import {LinkModel} from '../../../core/models/link.model';
 import {LinkType} from '../../../core/constants/consts';
+import {LinksData} from '../../../core/models/links-data.model';
 
-export const links: LinkModel[] = [
+export const LINKS: LinksData =
   {
-    text: 'Home',
-    url: '/',
-    type: LinkType.TEXT
-  },
-  {
-    text: 'About Us',
-    url: '/',
-    type: LinkType.TEXT
-  },
-  {
-    text: 'Prices',
-    url: '/',
-    type: LinkType.TEXT
-  },
-  {
-    text: 'Rules',
-    url: '/',
-    type: LinkType.TEXT
+    [LinkType.TEXT]: [
+      {
+        text: 'Home',
+        url: '/',
+        type: LinkType.TEXT
+      },
+      {
+        text: 'About Us',
+        url: '/',
+        type: LinkType.TEXT
+      },
+      {
+        text: 'Prices',
+        url: '/',
+        type: LinkType.TEXT
+      },
+      {
+        text: 'Rules',
+        url: '/',
+        type: LinkType.TEXT
+      }
+    ],
+    [LinkType.BUTTON]: [
+      {
+        text: 'Get Started Now',
+        url: '/',
+        type: LinkType.BUTTON
+      }
+    ]
   }
-]
+
