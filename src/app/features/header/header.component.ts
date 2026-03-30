719f96c103c55efaf72d9682d0b020f71c08e4f2
Sergey Malyuga <sergey@malyuga.ru>
2026-03-30 16:01:12 +0300

    feat(header): интегрировать ScreenService
    Интеграция:
      - подключён через inject()
    
    Класс:
      - добавлен сигнал isNavOpen
      - значение сигнала обновляется через сигнал сервиса isTablet()
    
    Шаблон:
      - элемент .header-main__wrapper-nav получает класс header-main__wrapper-nav-open при isNavOpen() === true
    
    Причина:
      - необходимо адаптивное поведение меню для планшетов (≤960px)

--- src/app/features/header/header.component.ts
+++ src/app/features/header/header.component.ts
@@ -1,6 +1,7 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {ChangeDetectionStrategy, Component, inject, signal} from '@angular/core';
 import {LINKS} from './data/links.data';
 import {LinkComponent} from '../../shared/components/link/link.component';
+import {ScreenService} from '../../core/services/screen.service';
 
 @Component({
   selector: 'app-header',
@@ -12,5 +13,11 @@ import {LinkComponent} from '../../shared/components/link/link.component';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HeaderComponent {
+  public screenService = inject(ScreenService);
   protected readonly LINKS_DATA = LINKS;
+  public isNavOpen = signal<boolean>(!this.screenService.isTablet());
+
+  public toggleNav() {
+    this.isNavOpen.set(!this.isNavOpen());
+  }
 }
