7c0a437e42c59377d42631669b9b6b4e6800e001
Sergey Malyuga <sergey@malyuga.ru>
2026-03-30 15:38:07 +0300

    feat(screen): добавить сервис разрешения экрана
    - ScreenService возвращает сигнал isTablet() для брейкпоинта ≤960px
    - Сигнал обновляется при resize окна

---
+++ src/app/core/services/screen.service.ts
@@ -0,0 +1,15 @@
+import {Injectable, signal} from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ScreenService {
+  private mediaQuery = window.matchMedia('(max-width: 960px)');
+  public isTablet = signal(this.mediaQuery.matches);
+
+  constructor() {
+    this.mediaQuery.addEventListener('change', (e) => {
+      this.isTablet.set(e.matches);
+    });
+  }
+}
