a0cfabef07be529b7dc52646cf6b32b74fccf4bb
Sergey Malyuga <sergey@malyuga.ru>
2026-03-31 11:33:54 +0300

    feat(about-vr): добавить информационный блок секции
    - Заголовок, подзаголовок и основной текст
    - Кнопка-ссылка (CTA)

--- src/app/features/about-vr/about-vr.component.ts
+++ src/app/features/about-vr/about-vr.component.ts
@@ -1,10 +1,16 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {LinkComponent} from '../../shared/components/link/link.component';
+import {LINKS} from '../../shared/data/links.data';
 
 @Component({
   selector: 'app-about-vr',
-  imports: [],
+  imports: [
+    LinkComponent
+  ],
   templateUrl: './about-vr.component.html',
   styleUrl: './about-vr.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AboutVrComponent {}
+export class AboutVrComponent {
+  protected readonly LINKS_DATA = LINKS;
+}
