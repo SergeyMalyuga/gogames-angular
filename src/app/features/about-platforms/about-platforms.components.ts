0b32e7ad86b6ac0ca0db3508892170e5cb557db1
Sergey Malyuga <sergey@malyuga.ru>
2026-03-31 12:52:14 +0300

    feat(about-platforms): добавить информационный блок для секции
    - Заголовок, подзаголовок и основной текст
    - Ссылка-кнопка (CTA)

--- src/app/features/about-platforms/about-platforms.components.ts
+++ src/app/features/about-platforms/about-platforms.components.ts
@@ -1,10 +1,16 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {LINKS} from '../../shared/data/links.data';
+import {LinkComponent} from '../../shared/components/link/link.component';
 
 @Component({
   selector: 'app-about-platforms',
-  imports: [],
+  imports: [
+    LinkComponent
+  ],
   templateUrl: './about-platforms.components.html',
   styleUrl: './about-platforms.components.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AboutPlatformsComponents {}
+export class AboutPlatformsComponents {
+  protected readonly LINKS_DATA = LINKS;
+}
