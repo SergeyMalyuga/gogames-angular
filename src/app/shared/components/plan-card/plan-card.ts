19e36862e9cc0f5c9d25dc8eece8e0413e254f6b
Sergey Malyuga <sergey@malyuga.ru>
2026-04-02 12:40:42 +0300

    feat(plan-card): добавить обработчик клика
    - Эмитит событие с карточкой при выборе
    - Используется для установки активной карточки

--- src/app/shared/components/plan-card/plan-card.ts
+++ src/app/shared/components/plan-card/plan-card.ts
@@ -1,12 +1,14 @@
-import {ChangeDetectionStrategy, Component, Input} from '@angular/core';
+import {ChangeDetectionStrategy, Component, EventEmitter, Input, Output} from '@angular/core';
 import {PlanCardModel} from '../../../core/models/plan-card.model';
 import {PLAN_LINKS} from './data/plan-links.data';
 import {LinkComponent} from '../link/link.component';
+import {NgClass} from '@angular/common';
 
 @Component({
   selector: 'app-plan-card',
   imports: [
-    LinkComponent
+    LinkComponent,
+    NgClass
   ],
   templateUrl: './plan-card.html',
   styleUrl: './plan-card.scss',
@@ -15,6 +17,11 @@ import {LinkComponent} from '../link/link.component';
 export class PlanCard {
   @Input({required: true}) card!: PlanCardModel;
   @Input({required: true}) activeCard!: PlanCardModel | null;
+  @Output() clicked = new EventEmitter<PlanCardModel>();
 
   public LINKS_DATA = PLAN_LINKS;
+
+  public onClick() {
+    this.clicked.emit(this.card);
+  }
 }
